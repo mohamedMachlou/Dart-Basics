7e77eb181dfcc4bdfefa7405c741d5d058a88e07
Mohamed Machlou <medmachlou@gmail.com>
2024-04-18 16:44:02 +0100

    Dart Basics Add Conditionnelle Structure Switch Case

--- conditionnelle.dart
+++ conditionnelle.dart
@@ -23,43 +23,65 @@
 
 // Exercice 3
 
+// import 'dart:io';
+
+// void main() {
+//   double prime;
+
+//   print('Taper Votre Grade \"Tech\" ou \"Ing\" : ');
+//   String grade = stdin.readLineSync()!;
+//   print('Taper Votre Salaire : ');
+//   double slr = double.parse(stdin.readLineSync()!);
+
+//   if (grade == "Tech" && slr < 3000) {
+//     prime = 0.02;
+//   } else {
+//     if (grade == "Tech" && slr < 4500) {
+//       prime = 0.025;
+//     } else {
+//       prime = 0.03;
+//     }
+//   }
+//   if (grade == "Ing" && slr < 5000) {
+//     prime = 0.035;
+//   } else {
+//     if (grade == "Ing" && slr < 6500) {
+//       prime = 0.04;
+//     } else {
+//       prime = 0.05;
+//     }
+//   }
+
+//   if (grade != "Tech" || grade != "Ing") {
+//     prime = 0.015;
+//   }
+
+//   prime = prime * slr;
+//   slr = slr + prime;
+//   String slrForma = slr.toStringAsFixed(2);
+
+//   print(
+//       ' Votre garde est : $grade \n Votre prime est : $prime \n Votre Salire est: $slrForma');
+// }
 import 'dart:io';
 
 void main() {
-  double prime;
+  print('Taper un nombre : ');
+  int n = int.parse(stdin.readLineSync()!);
 
-  print('Taper Votre Grade \"Tech\" ou \"Ing\" : ');
-  String grade = stdin.readLineSync()!;
-  print('Taper Votre Salaire : ');
-  double slr = double.parse(stdin.readLineSync()!);
+  switch (n) {
+    case < 4:
+      print("1");
+      break;
 
-  if (grade == "Tech" && slr < 3000) {
-    prime = 0.02;
-  } else {
-    if (grade == "Tech" && slr < 4500) {
-      prime = 0.025;
-    } else {
-      prime = 0.03;
-    }
+    case < 10:
+      print("2");
+      break;
+
+    case < 100:
+      print("3");
+      break;
+    default:
+      print('Default');
   }
-  if (grade == "Ing" && slr < 5000) {
-    prime = 0.035;
-  } else {
-    if (grade == "Ing" && slr < 6500) {
-      prime = 0.04;
-    } else {
-      prime = 0.05;
-    }
-  }
-
-  if (grade != "Tech" || grade != "Ing") {
-    prime = 0.015;
-  }
-
-  prime = prime * slr;
-  slr = slr + prime;
-  String slrForma = slr.toStringAsFixed(2);
-
-  print(
-      ' Votre garde est : $grade \n Votre prime est : $prime \n Votre Salire est: $slrForma');
 }
